98c65dabb0bb6e13b61567e5f00181283e794db2
Dipali Lalu <dvlalu7@gamil.com>
2024-07-22 14:49:17 +0530

    java variable let, var, const

---
+++ Java script/variable.js
@@ -0,0 +1,19 @@
+a = 10;
+nm = "dipali";
+console.log("name is", nm);
+const b = 20;
+console.log("a+b is", a + b);
+a = 20;
+console.log("a+b is", a + b);
+// b=10; 
+var nm;
+// let a=30;
+console.log(nm);
+let x=10;
+console.log(x);
+{
+    let x=20;
+    console.log(x);
+}
+console.log(x);
+
