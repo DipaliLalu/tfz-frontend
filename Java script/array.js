b3cc5c00f103b013ebf6f408984c20927c76ecc8
Dipali Lalu <dvlalu7@gamil.com>
2024-07-27 18:09:34 +0530

    array methods in js

--- Java script/array.js
+++ Java script/array.js
@@ -19,11 +19,50 @@ let num=[23,60,11,5,4];
 // let ans=num.sort(compare);
 // console.log(ans);
 // console.log(ans.reverse());
-console.log(num.splice(1,3,10,11,12));
-console.log(num.slice(2,5)); // it is create a new array so it's not effect an original array
-console.log(num);
+// console.log(num.splice(1,3,10,11,12)); //effect a original array
+// console.log(num.slice(2,5)); // it is create a new array so it's not effect an original array
+// console.log(num.fill(0,2));
+// console.log(num);
 
 // console.log(arr.length);
 // console.log(arr);
+
+let arrs=Array.from('dipali');
+// console.log(arrs)
+ 
+for (let i = 0; i < arrs.length; i++) {
+    const element = arrs[i];
+    // console.log(element);
+}
+arrs.forEach((e)=>{
+    // console.log(e);
+})
+
+for (const iterator of arrs) {
+    // console.log(iterator);
+}
+
+for (const key in arrs) {
+// console.log(key);//return a key of array
+//    console.log(arrs[key]);//return a value
+}
+
+let a=[1,3,4,2,5];
+let a1=a.map((value,index,arr)=>{
+    // console.log(value,index,arr);
+    return value*index;
+});
+console.log(a1);
+ 
+let a2=a.filter((value)=>{
+    return value;
+})
+console.log(a2);
+ let add=(a,b)=>{
+    return a+b
+ }
+let a3=a.reduce((add));
+console.log(a3);
+console.log(a);
 
 
