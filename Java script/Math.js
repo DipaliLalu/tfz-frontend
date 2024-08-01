e1eabcc852cdd81ad7d191f22f0d8b3eb512c69a
Dipali Lalu <dvlalu7@gamil.com>
2024-08-01 18:02:17 +0530

    Math and Date object in js

---
+++ Java script/Math.js
@@ -0,0 +1,40 @@
+console.log("Math Object in js");
+
+//Property to Math Object
+let m=Math;
+// console.log(m);
+console.log("The value od Math.PI: ",m.PI);
+console.log("returns the square root of 2: ",m.SQRT2);
+
+//Math Methods
+let a=5.5;
+let b=10.7;
+
+console.log("Rounded the value of a: ",Math.round(a));
+console.log("Rounded the value of b: ",Math.round(b));
+
+console.log("5.8 rounded up to nearest integer is: ",Math.ceil(5.8));
+console.log("5.8 rounded down to nearest integer is: ",Math.floor(5.8));
+
+console.log('integer value of 88.4 is',Math.trunc(88.4));
+console.log('integer value of 71.9 is',Math.trunc(71.9));
+
+console.log('8 to the power of 2 is: ',Math.pow(8,2));
+console.log('2 to the power of 3 is: ',Math.pow(2,3));
+
+console.log('the square root of 64 is: ',Math.sqrt(64));
+console.log('the square root of 36 is: ',Math.sqrt(36));
+
+console.log('absolute value of 55.7 is: ',Math.abs(55.7));
+console.log('absolute value of -55.7 is: ',Math.abs(-55.7));
+
+console.log('minimum value of 4,3,5,2 is: ',Math.min(4,3,5,2));
+console.log('minimum value of -4,3,5,-200 is: ',Math.min(-4,3,5,-200));
+
+console.log('maximum value of 4,3,5,2 is: ',Math.max(4,3,5,2));
+console.log('maximum value of -4,3,5,-200 is: ',Math.max(-4,3,5,-200));
+
+console.log('random number is: ',Math.random());
+console.log('random number is: ',Math.floor(Math.random()*100));
+console.log('random number is: ',Math.floor(Math.random()*100)+1);
+
