4c7532e7c63456f31960b8414f09e0305c1d1562
Dipali Lalu <dvlalu7@gamil.com>
2024-09-06 19:01:42 +0530

    form validation in js

--- JavaScript/event_practice/form_validation.js
+++ JavaScript/event_practice/form_validation.js
@@ -4,8 +4,16 @@ let pwd2 = document.getElementById("password2");
 let email = document.getElementById("email");
 let errorMessage = document.getElementById("error-msg");
 const error = {};
+let data = [];
+let un;
+let obj = JSON.parse(localStorage.getItem("user"));
+console.log(obj);
 
-
+if(obj !== null){
+    un=obj.map((e)=>{
+        return e.uname;
+    })
+}
 function required() {
     let regex = /[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]/i;
     if (uname.value.trim() == "") {
@@ -20,38 +28,56 @@ function required() {
     if (email.value.trim() == "") {
         error.email = "Enter email";
 
-    }else if(!regex.test(email.value.trim())){
-        error.emailCheck="please enter valid eamil format";
+    } else if (!regex.test(email.value.trim())) {
+        error.emailCheck = "please enter valid eamil format";
     }
     return error;
 }
 
-function checkPassword(){
-    if(pwd1.value.trim() !== pwd2.value.trim()){
-        error.pwd="please enter same password";
+function checkPassword() {
+    if (pwd1.value.trim() !== pwd2.value.trim()) {
+        error.pwd = "please enter same password";
     }
+}
+
+function uniqueUsername() {
+    if(obj !== null){
+    un.forEach((e) => {
+        if (uname.value.trim() == e) {
+            error.uniqueUsername = "Username already exists";
+        }
+    })}
 }
 
 document.querySelector("form").addEventListener("submit", async (e) => {
     e.preventDefault();
     required();
+    uniqueUsername();
     checkPassword();
+
     if (Object.keys(error).length !== 0) {
         // console.log(error);
-        let msg=document.getElementById("error-msg");
-        msg.style.display="block";
+        let msg = document.getElementById("error-msg");
+        msg.style.display = "block";
         Object.values(error).forEach((e) => {
-            msg.innerHTML += e+ "<br>";
+            msg.innerHTML += e + "<br>";
         });
-    }else{
-        let value={}
-        value.uname=uname.value.trim();
-        value.pwd=pwd1.value.trim();
-        value.email=email.value.trim();
-        console.log(value);
-        localStorage.setItem("user",JSON.stringify(value));
-        location.href="./login.html";
-    }
 
+    } else {
+        let value = {}
+        value.uname = uname.value.trim();
+        value.pwd = pwd1.value.trim();
+        value.email = email.value.trim();
+        if (obj==null) {
+            data.push(value);
+            localStorage.setItem("user", JSON.stringify(data));
+            location.href = "./login.html";
+        } else {
+            obj.push(value);
+            localStorage.setItem("user", JSON.stringify(obj));
+            location.href = "./login.html";
+        }
+        console.log(obj);
+    }
 });
 
